1804b056170055fb96b346e271e7d11293034c58
mah <zanzinzzo@gmail.com>
2023-05-13 16:21:14 -0300

    error checks

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,23 @@
-def arithmetic_arranger(problems):
+def arithmetic_arranger(problems, show_result = False):
+    if len(problems) > 5:
+        return 'Error: Too many problems.'
+    
+    arranged_problems, top_line, down_line, dash_line = ""
+    for problem in problems:
+        inputs = problem.split()
 
+        # error checking 
+        if len(inputs[0]) > 4 or len(inputs[2]) > 4: 
+            return 'Error: Numbers cannot be more than four digits.'
 
+        if inputs[1] != "-" and inputs[1] != "+":
+            return "Error: Operator must be '+' or '-'."
+        
+        try:
+            inputs[0] = int(inputs[0])
+            inputs[2] = int(inputs[2])
+        except:
+            return 'Error: Numbers must only contain digits.' 
+             
     return arranged_problems
+
