05b1b580063ec1522bf5dae2a9cea7a455fc33ff
mah <zanzinzzo@gmail.com>
2023-05-15 13:42:14 -0300

    alignment corrected

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -30,16 +30,16 @@ def arithmetic_arranger(problems, show_result = False):
         len_diff = len(str(val1)) - len(str(val2))
         if len_diff < 0:
             len_diff = len_diff * -1
-            top_line = top_line + space_filler(len_diff) + val1 
+            top_line = top_line + space_filler(len_diff + 2) + val1 
             down_line = down_line + operator + " " + val2 
-            dash_line = space_filler(len(str(val2))+2, "-")
+            dash_line = space_filler(len(str(val2)) + 2, "-")
         else:
-            top_line = top_line + space_filler(len_diff + 1)+ val1 + "    "
-            down_line = down_line + operator + " " + space_filler(len_diff) + val2 + "    "
-            dash_line = space_filler(len(str(val2))+len_diff+ 2, "-") + "    "  
+            top_line = top_line + "  " + val1
+            down_line = down_line + operator + " " + space_filler(len_diff) + val2 
+            dash_line = space_filler(len(str(val1)) + 2, "-")
 
-        if i == (len(problems)):
-            top_line = top_line + "    "
+        if i+1 < len(problems):
+            top_line  = top_line  + "    "
             down_line = down_line + "    "
             dash_line = dash_line + "    "
     arranged_problems = "\n".join([top_line, down_line, dash_line])
@@ -54,4 +54,4 @@ def space_filler(number, char = " "):
         idx = idx + 1
     return filling 
 
-print(arithmetic_arranger(["356 - 1369" ], False))
+print(arithmetic_arranger(["3 - 100" ], False))
