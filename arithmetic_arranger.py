8818e767840cbf430e35953c9d4522e0a8b86c19
mah <zanzinzzo@gmail.com>
2023-05-15 10:10:40 -0300

    now shows the numbers and the lines, just not right oriented as the problem requests

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -2,7 +2,11 @@ def arithmetic_arranger(problems, show_result = False):
     if len(problems) > 5:
         return 'Error: Too many problems.'
     
-    arranged_problems, top_line, down_line, dash_line = ""
+    arranged_problems = "" 
+    top_line = "" 
+    down_line = "" 
+    dash_line = ""
+
     for problem in problems:
         inputs = problem.split()
 
@@ -18,6 +22,36 @@ def arithmetic_arranger(problems, show_result = False):
             inputs[2] = int(inputs[2])
         except:
             return 'Error: Numbers must only contain digits.' 
-             
+        
+        
+    for i in range(0, len(problems)):
+        arranged_problems = arranged_problems+problems[i]+"\n"
+        val1, operator, val2 = problems[i].split()
+        len_diff = len(str(val1)) - len(str(val2))
+        if len_diff < 0:
+            len_diff = len_diff * -1
+            top_line = top_line + space_filler(len_diff) + val1 
+            down_line = down_line + operator + " " + val2 
+            dash_line = space_filler(len(str(val2))+2, "-")
+        else:
+            top_line = top_line + space_filler(len_diff + 1)+ val1 + "    "
+            down_line = down_line + operator + " " + space_filler(len_diff) + val2 + "    "
+            dash_line = space_filler(len(str(val2))+len_diff+ 2, "-") + "    "  
+
+        if i == (len(problems)):
+            top_line = top_line + "    "
+            down_line = down_line + "    "
+            dash_line = dash_line + "    "
+    arranged_problems = "\n".join([top_line, down_line, dash_line])
+
     return arranged_problems
 
+def space_filler(number, char = " "):
+    filling = ""
+    idx = 0
+    while idx < number:
+        filling = filling + char
+        idx = idx + 1
+    return filling 
+
+print(arithmetic_arranger(["356 - 1369" ], False))
