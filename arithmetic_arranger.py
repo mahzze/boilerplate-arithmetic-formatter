bb87809dc98c6b371d0d2db91572a0dafd5cff5c
mah <zanzinzzo@gmail.com>
2023-05-15 13:44:34 -0300

    dashline fixed

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -32,16 +32,17 @@ def arithmetic_arranger(problems, show_result = False):
             len_diff = len_diff * -1
             top_line = top_line + space_filler(len_diff + 2) + val1 
             down_line = down_line + operator + " " + val2 
-            dash_line = space_filler(len(str(val2)) + 2, "-")
+            dash_line = dash_line + space_filler(len(str(val2)) + 2, "-")
         else:
             top_line = top_line + "  " + val1
             down_line = down_line + operator + " " + space_filler(len_diff) + val2 
-            dash_line = space_filler(len(str(val1)) + 2, "-")
+            dash_line = dash_line + space_filler(len(str(val1)) + 2, "-")
 
         if i+1 < len(problems):
             top_line  = top_line  + "    "
             down_line = down_line + "    "
             dash_line = dash_line + "    "
+
     arranged_problems = "\n".join([top_line, down_line, dash_line])
 
     return arranged_problems
@@ -54,4 +55,4 @@ def space_filler(number, char = " "):
         idx = idx + 1
     return filling 
 
-print(arithmetic_arranger(["3 - 100" ], False))
+print(arithmetic_arranger(["3 - 100", "300 + 1000" ], False))
